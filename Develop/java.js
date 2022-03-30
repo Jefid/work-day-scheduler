9618858dfdbf8b7caae491539af645683527151f
Jefid <99925114+Jefid@users.noreply.github.com>
2022-03-30 16:05:12 -0400

    timeslots display, still need to add localstorage through the save button

--- Develop/java.js
+++ Develop/java.js
@@ -12,7 +12,7 @@ var hourBlocks = [
 //variable to save current time to reference if time slot has passed
 var currentTime = moment().format("HH") + ":00";
 
-var domContainer = document.querySelector("#container");
+var domContainer = document.querySelector("#timeslot-container");
 //inserting time and day and time into Current day
 
 window.setInterval(function () {
@@ -26,26 +26,62 @@ var displayTimesBlocks = function () {
   for (i = 0; i < hourBlocks.length; i++) {
     //creates div to hold task
     var taskRow = document.createElement("div");
-    taskRow.classList = "row time-block";
+    taskRow.classList = "row time-block justify-content-center";
     //sets id of each elemnt to index value
     taskRow.id = hourBlocks.indexOf(hourBlocks[i]);
 
     //create timeslot
 
     var timeSlot = document.createElement("h4");
-    timeSlot.classList = "hour";
+    timeSlot.classList = "hour col-md-2";
     timeSlot.id = hourBlocks.indexOf(hourBlocks[i]);
     timeSlot.textContent = hourBlocks[i];
     //appending to task row
     taskRow.appendChild(timeSlot);
 
     //create input feild
-    var taskInput = document.createElement("input")
+    var taskInput = document.createElement("input");
+    taskInput.classList = "time-block clearable col-md-9 description p-0";
+    taskInput.id = "input" + hourBlocks.indexOf(hourBlocks[i]);
 
+//space for local storage if statement to fill out info with parsed local storage
+
+//adds task info to middle of taskrow
+    taskRow.appendChild(taskInput);
+
+
+    //create save button
+    var saveBtn = document.createElement("button");
+    saveBtn.classList = "saveBtn col-md-1";
+    saveBtn.id = "btn" + hourBlocks.indexOf(hourBlocks[i]);
+    saveBtn.innerHTML = "<i class='far fa-save fa-lg'></i>";
+    
+    //adds save button to task row
+    taskRow.appendChild(saveBtn);
 
 //adding created elements to the form displayed
     domContainer.appendChild(taskRow);
+
+    if (currentTime === hourBlocks[i]){
+    taskInput.classList = "present col-md-9 description p-0"
+    }
+    
+    if (currentTime < hourBlocks[i]){
+        taskInput.classList = "future col-md-9 description p-0"
+        }
+        
+    if (currentTime > hourBlocks[i]){
+        taskInput.classList = "past col-md-9 description p-0"
+        }
   }
 };
+
+//need to add code for save button
+
+//need to add function to grab local storage
+
+
+
+
 
 displayTimesBlocks();
